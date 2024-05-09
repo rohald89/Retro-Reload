e92775e20c7ba9e0a52745e34696c689ac72017b
Rohald van Merode <rohald_89@live.nl>
2024-05-09 20:34:25 +0200

    added Google Auth

--- app/root.tsx
+++ app/root.tsx
@@ -273,10 +273,10 @@ function Logo() {
 	return (
 		<Link to="/" className="group grid leading-snug">
 			<span className="font-light transition group-hover:-translate-x-1">
-				epic
+				Retro
 			</span>
 			<span className="font-bold transition group-hover:translate-x-1">
-				notes
+				Reload
 			</span>
 		</Link>
 	)
